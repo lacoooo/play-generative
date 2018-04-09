0715c84abe486d0d71486e19670b85acd85131d9
Neko <469867876@qq.com>
2018-04-09 14:32:00 +0800

    kdtree

--- 2d/tree/kdtree/kdtree.js
+++ 2d/tree/kdtree/kdtree.js
@@ -1,6 +1,6 @@
 ;
-
 class Node {
+
     constructor(x, y) {
         this.x = x
         this.y = y
@@ -9,10 +9,33 @@ class Node {
         this.dimension = null
         this.collectionLeft = []
         this.collectionRight = []
+        this.parent = ''
     }
-    add ( newNode ) {
+
+    add() {
+        let dimension = 0
+        if (this.dimension) dimension = 0
+        else dimension = 1
+        let centerLeft = tree.getCenter(this.collectionLeft, dimension)
+        if (centerLeft) {
+            centerLeft.parent = this
+            if (!this.left) {
+                this.left = centerLeft
+            }
+            this.left.add()
+        }
+
+        let centerRight = tree.getCenter(this.collectionRight, dimension)
+        if (centerRight) {
+            centerRight.parent = this
+            if (!this.right) {
+                this.right = centerRight
+            }
+            this.right.add()
+        }
 
     }
+
 }
 
 // 二维kdtree
@@ -27,7 +50,7 @@ class Kdtree {
     }
 
     produceNodes(num = 10) {
-        for (let i = 0; i < num; i ++) {
+        for (let i = 0; i < num; i++) {
             const x = Math.random() * this.size.sizeX
             const y = Math.random() * this.size.sizeY
             this.collection.push(new Node(x, y))
@@ -37,6 +60,7 @@ class Kdtree {
 
     getCenter(collection, dimension) {
         const len = collection.length
+        if (!len || len < 2) return false
         let total = 0
         for (let i = 0; i < len; i++) {
             if (dimension == 0) total += collection[i].x
@@ -44,36 +68,42 @@ class Kdtree {
         }
         const centerNum = total / len
         let center = null
+        let defferMin = 99999999999999
         let index = 0
-        
+
         let num
         if (dimension == 0) num = 'x'
         else if (dimension == 1) num = 'y'
-        collection.map( (ele, i) => {
-            const differ = Math.min( Math.abs(ele[num] - centerNum) )
-            if ( center == null || differ < center[num] ) {
+
+        collection.map((ele, i) => {
+            const deffer = Math.abs(ele[num] - centerNum)
+            if (deffer < defferMin) {
+                defferMin = deffer
                 center = ele
                 index = i
             }
-        } )
-        collection.map( (ele, i) => {
+        })
+        collection.map((ele, i) => {
             if (i == index) return
+            if (!ele[num] || !center[num]) return
             if (ele[num] < center[num]) center.collectionLeft.push(ele)
             else if (ele[num] > center[num]) center.collectionRight.push(ele)
-        } )
-        
+        })
+        // center.centerNum = centerNum + ', ' + center[num]
         return center
     }
 
     getNode(dimension) {
         let center = this.getCenter(this.collection, dimension)
-        this.addToTree( center )
+        this.addToTree(center, dimension)
     }
 
-    addToTree( newNode ) {
-        if (!this.root) this.root = newNode
-        else this.root.add( newNode )
+    addToTree(newNode, dimension) {
+        newNode.dimension = dimension
+        this.root = newNode
+        this.root.add()
     }
+
 }
 
 let tree = new Kdtree(1000, 1000).produceNodes(100)
