0961316a80724f5c414485f80f2151cb24c3e827
Neko <469867876@qq.com>
2018-05-30 18:08:29 +0800

    1

---
+++ 2d/magnetic/draw4/particle.js
@@ -0,0 +1,53 @@
+
+class Particle {
+
+    constructor(x, y) {
+        this.x = x || floor(random() * width)
+        this.y = y || floor(random() * height)
+        this.v = random(1, 10)
+        this.vx = random(1, 10)
+        this.vy = random(1, 10)
+        this.c = baseImageData[floor(this.x) + floor(this.y) * width]
+    }
+
+    magic(poles) {
+        let dists = []
+        let totalGravity = 0
+        poles.map(ele => {
+            const d = dist(this.x, this.y, ele.x, ele.y)
+            // if (d > 400) return
+            dists.push({d: d, g: ele.gravity})
+            totalGravity += abs(ele.gravity)
+        })
+        const distsA = dists.filter(ele => ele.g >= 0)
+        if (distsA.some(ele => ele.d < random(1, 40))) {
+            // this.x = floor(random() * width)
+            // this.y = floor(random() * height)
+        }
+        else {
+            let x, y
+            dists.map( (ele, i) => {
+                x = poles[i].x - this.x
+                y = poles[i].y - this.y
+                const drift = ele.g / totalGravity / ele.d * 4
+                this.x += x * drift
+                this.y += y * drift
+            })
+            let c = baseImageData[floor(this.x) + floor(this.y) * width]
+            let v = c && c[3] / 50 || 0.2
+            this.x += cos(v) / 3
+            this.y += sin(v) / 3
+            if (random() > 0.9) this.c = c && `rgba(${c[0]}, ${c[1]}, ${c[2]}, 1)`
+        }
+
+        if (this.x >= width) this.x = 0
+        if (this.y >= height) this.y = 0
+    }
+
+    draw(poles) {
+        if (poles.length > 0) this.magic(poles)
+        fill(this.c || 0)
+        ellipse(this.x, this.y, 2.5, 2.5)
+    }
+
+}
