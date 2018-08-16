04860fb98faa076c0edefb3bcda1ab9e9978c8c5
Neko <469867876@qq.com>
2018-08-16 14:01:59 +0800

    1

--- 3d/geometry/g1.js
+++ 3d/geometry/g1.js
@@ -1,51 +1,68 @@
 let simplexNoise = new SimplexNoise()
-let time = 1
-threeSetting.setBgColor(`rgba(200,200,200,1)`)
+let time = 0
+threeSetting.setBgColor(`rgba(220,220,220,1)`)
 let boxes = []
 function updateVertices() {
 
     boxes.map(b => {
-        b.position = JSON.parse(JSON.stringify(b._o))
-        b.rotation = JSON.parse(JSON.stringify(b._r))
+        b.position = {
+            x: b._o.x,
+            y: b._o.y,
+            z: b._o.z
+        }
+        b.position.multiplyScalar = b._o.multiplyScalar
+        b.rotation = {x: 0, y: 0, z: 0}
         var perlin = simplexNoise.noise3D(
-            (b.position.x * 0.002) + (time * 0.006),
-            (b.position.y * 0.002),
-            (b.position.z * 0.002)
+            (b.position.x * 0.002) + time,
+            (b.position.y * 0.002) + time,
+            (b.position.z * 0.002) + time
         )
-        var ratio = perlin / 200
-        b.position.multiplyScalar(ratio + 1)
-        b.rotation.x += perlin / 4
-        // b.scale.y += perlin / 40
+        // perlin *= perlin
+        b.position.multiplyScalar(perlin / 1000 + 1)
+        b.rotation.x += perlin / 40
+        b.rotation.y += perlin / 40
+        b.rotation.z += perlin / 40
     })
-    time += 1
+    time += 0.001
 }
 
 function setup() {
-    light = new THREE.HemisphereLight(`rgba(200,200,200,1)`, `rgba(10,10,10,1)`, 0.3);
+    light = new THREE.HemisphereLight(`rgba(255,255,255,1)`, `rgba(210,210,210,1)`, 0.6);
     scene.add(light);
 
-    light = new THREE.DirectionalLight(`rgba(30,30,30,1)`, 0.3);
+    light = new THREE.DirectionalLight(`rgba(240,240,240,1)`, 0.5);
     light.position.set(200, 300, 400);
     scene.add(light);
 
     material = new THREE.MeshPhongMaterial({
-        emissive: `rgba(10,10,10,1)`,
-        emissiveIntensity: 0.2,
+        emissive: `rgba(150,150,150,1)`,
+        emissiveIntensity: 0.1,
         shininess: 0
     });
 
-    geometry = new THREE.IcosahedronGeometry(100, 4)
+    geometry = new THREE.IcosahedronGeometry(80, 4)
     for (var i = 0; i < geometry.vertices.length; i++) {
         var v = geometry.vertices[i];
-        v._o = v.clone();
 
-        let box = new THREE.BoxGeometry(6, 1, 6)
+        let box = new THREE.BoxGeometry(10, 0.6, 2)
         let shape = new THREE.Mesh(box, material)
+
         shape.position.x = v.x
         shape.position.y = v.y
         shape.position.z = v.z
-        shape._o = JSON.parse(JSON.stringify(shape.position))
-        shape._r = JSON.parse(JSON.stringify(shape.rotation))
+        shape._o = {
+            x: shape.position.x,
+            y: shape.position.y,
+            z: shape.position.z
+        }
+        shape._o.multiplyScalar = shape.position.multiplyScalar
+        var same
+        boxes.map(ele => {
+            if (ele.position.x == shape.position.x &&
+            ele.position.y == shape.position.y &&
+            ele.position.z == shape.position.z) same = true
+        })
+        if (same) continue
         scene.add(shape)
         boxes.push(shape)
     }
