5cd2f6bf985b854dc2ed6bf3ccf7ff899ac85e98
neko <469867876@qq.com>
2018-09-02 02:48:20 +0800

    1

--- 3d/paper/1/index.js
+++ 3d/paper/1/index.js
@@ -1,5 +1,10 @@
 let simplexNoise = new SimplexNoise()
 let time = 0
+let lines = []
+let boxes = []
+
+threeSetting.setBgColor(`rgba(200,0,0,1)`)
+threeSetting.setCamera(true)
 
 var options = (() => {
 
@@ -8,9 +13,9 @@ var options = (() => {
             this.x = 0
             this.y = 0
             this.z = 0
-            this.px = 0
-            this.py = 0
-            this.pz = 0
+            this.px = 48
+            this.py = 200
+            this.pz = -200
         }
     }
 
@@ -18,9 +23,9 @@ var options = (() => {
 
     var gui = new dat.GUI()
 
-    gui.add(options, 'x', 0, 2)
-    gui.add(options, 'y', 0, 2)
-    gui.add(options, 'z', 0, 2)
+    gui.add(options, 'x', -2, 2)
+    gui.add(options, 'y', -2, 2)
+    gui.add(options, 'z', -2, 2)
 
     gui.add(options, 'px', -200, 200)
     gui.add(options, 'py', -200, 200)
@@ -30,18 +35,18 @@ var options = (() => {
 
 })()
 
-threeSetting.setBgColor(`rgba(200,200,200,1)`)
-// threeSetting.setCamera(true)
-
-let boxes = []
 function updateVertices() {
+    shapeL.rotation.x = options.x
+    shapeL.rotation.y = options.y
+    shapeL.rotation.z = options.z
+
     shape.rotation.x = options.x
     shape.rotation.y = options.y
     shape.rotation.z = options.z
-    
-    shape2.rotation.x = options.x
-    shape2.rotation.y = options.y
-    shape2.rotation.z = options.z
+
+    group.rotation.x = options.x
+    group.rotation.y = options.y
+    group.rotation.z = options.z
 
     light.position.x = options.px
     light.position.y = options.py
@@ -54,31 +59,56 @@ function updateVertices() {
             (v.y * 0.03),
             (v.z * 0.03)
         )
+        var perlin2 = simplexNoise.noise3D(
+            (v.x * 0.005) + time,
+            (v.y * 0.005),
+            (v.z * 0.005)
+        )
         // var ratio = (perlin / 20 + 1)
         // v.multiplyScalar(ratio)
-        v.z = perlin * 10
+        v.z = perlin * 15 + perlin2 * 80
+    })
+
+    geometryL.vertices.map((v, i) => {
+        v.copy(v._o)
+        var perlin = simplexNoise.noise3D(
+            (v.x * 0.03) + time,
+            (v.y * 0.03),
+            (v.z * 0.03)
+        )
+        var perlin2 = simplexNoise.noise3D(
+            (v.x * 0.005) + time,
+            (v.y * 0.005),
+            (v.z * 0.005)
+        )
+        // var ratio = (perlin / 20 + 1)
+        // v.multiplyScalar(ratio)
+        v.z = perlin * 15 + perlin2 * 80
+        lines[i].geometry.vertices[0].z = v.z + 30
+        lines[i].geometry.verticesNeedUpdate = true;
     })
 
     time += 0.004
     geometry.verticesNeedUpdate = true;
+    geometryL.verticesNeedUpdate = true;
 }
 
 function setup() {
-    light = new THREE.HemisphereLight(`rgba(255,255,255,1)`, `rgba(210,210,210,1)`, 0.7)
+    light = new THREE.HemisphereLight(`rgba(255,255,255,1)`, `rgba(210,210,210,1)`, 0)
     scene.add(light)
 
-    light = new THREE.DirectionalLight(`rgba(220,220,220,1)`, 0.6)
+    light = new THREE.DirectionalLight(`rgba(210,210,210,1)`, 0)
     light.position.set(options.px, options.py, options.pz)
     scene.add(light)
 
     material = new THREE.MeshPhongMaterial({
-        emissive: `rgba(150,150,150,1)`,
-        emissiveIntensity: 0.1,
+        emissive: `rgba(0,0,0,1)`,
+        emissiveIntensity: 0.2,
         flatShading: true,
         shininess: 0
     })
 
-    geometry = new THREE.PlaneGeometry(100, 100, 12, 12)
+    geometry = new THREE.PlaneGeometry(500, 500, 80, 80)
     for (var i = 0; i < geometry.vertices.length; i++) {
         var v = geometry.vertices[i]
         // v.z = 100
@@ -87,6 +117,32 @@ function setup() {
     shape = new THREE.Mesh(geometry, material)
     shape.position.z = 40
     scene.add(shape)
+
+    // shape.visible = false
+
+    geometryL = new THREE.PlaneGeometry(500, 500, 80, 80)
+    var materialL = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true, transparent: false, opacity: 0.5 });
+    var lm = new THREE.LineBasicMaterial({ color: 0xffffff, linewidth: 3 })
+    group = new THREE.Group();
+    for (var i = 0; i < geometryL.vertices.length; i++) {
+        var v = geometryL.vertices[i]
+        // v.z = 100
+        v._o = v.clone()
+        var q = new THREE.Geometry();
+        var line = new THREE.Line(q, lm);
+        q.vertices.push(new THREE.Vector3(v.x, v.y, 0));
+        q.vertices.push(new THREE.Vector3(v.x, v.y, -500));
+        if (!(i % 4)) group.add(line)
+        lines.push(line)
+    }
+
+    scene.add(group)
+
+    shapeL = new THREE.Mesh(geometryL, materialL)
+    shapeL.position.z = 70
+    scene.add(shapeL)
+
+    // shapeL.visible = false
 
     material2 = new THREE.MeshPhongMaterial({
         color: `rgba(180,180,180,1)`,
@@ -100,6 +156,7 @@ function setup() {
     shape2 = new THREE.Mesh(geometry2, material2)
     shape2.position.z = 0
     scene.add(shape2)
+    shape2.visible = false
 
 }
 
