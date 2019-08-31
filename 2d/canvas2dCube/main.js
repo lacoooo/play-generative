fd982ff7c8b6f1e5c9257a0e4feede43c6d45802
neko <469867876@qq.com>
2019-08-31 18:05:49 +0800

    1

--- 2d/canvas2dCube/main.js
+++ 2d/canvas2dCube/main.js
@@ -1,5 +1,5 @@
 {       
-var simplex = new SimplexNoise()
+// var simplex = new SimplexNoise()
 
 class Coptions {
     constructor(w, h) {
@@ -10,60 +10,101 @@ class Coptions {
     }
 }
 
-var co = new Coptions(1000, 1000)
+var co = new Coptions(600, 600)
 
 window.cubes = []
 
 class Vect {
-    constructor (x, y, z, l) {
-        this.x = x
-        this.y = y
-        this.z = z
+    constructor (angel, l) {
+        this._angel = angel
         this.l = l
+        this.frame = 0
     }
 
-    get2dPos() {
+    get pos2d () {
+        let x = Math.sin(this._angel * Math.PI / 180 + this.frame * 0.01) * this.l
+        let y = this.l / Math.sin(45 * Math.PI / 180) * 0.5 * this.index
+        let z = Math.cos(this._angel * Math.PI / 180 + this.frame * 0.01) * this.l
+        x = x * 0.8 + x * z / this.l * 0.2
+        y = y * 0.8 + y * z / this.l * 0.2
         return {
-            x: 0
+            x, y
         }
+    }
+
+    update() {
+        this.frame ++
+    }
+}
+
+class Face {
+    constructor (index) {
+        this.index = index
+        this.vectors = []
+    }
+    addVect (vector) {
+        vector.index = this.index
+        this.vectors.push(vector)
     }
 }
 
 class Cube {
     constructor (length) {
         this.l = length
-        this.vectors = []
-        this.initVectors()
+        this.faces = []
+        this.initFaces()
     }
-    initVectors () {
-        this.vectors.push(new Vect(1, 1, 1, this.l))
-        this.vectors.push(new Vect(1, -1, 1, this.l))
-        this.vectors.push(new Vect(1, 1, -1, this.l))
-        this.vectors.push(new Vect(1, -1, -1, this.l))
+    initFaces () {
+        this.faces.push(new Face(-1))
+        this.faces.push(new Face(1))
+        
+        this.faces[0].addVect(new Vect(0, this.l))
+        this.faces[0].addVect(new Vect(90, this.l))
+        this.faces[0].addVect(new Vect(180, this.l))
+        this.faces[0].addVect(new Vect(270, this.l))
+        this.faces[0].addVect(new Vect(0, this.l))
 
-        this.vectors.push(new Vect(-1, 1, 1, this.l))
-        this.vectors.push(new Vect(-1, -1, 1, this.l))
-        this.vectors.push(new Vect(-1, 1, -1, this.l))
-        this.vectors.push(new Vect(-1, -1, -1, this.l))
+        this.faces[1].addVect(new Vect(0, this.l))
+        this.faces[1].addVect(new Vect(90, this.l))
+        this.faces[1].addVect(new Vect(180, this.l))
+        this.faces[1].addVect(new Vect(270, this.l))
+        this.faces[1].addVect(new Vect(0, this.l))
     }
     draw () {
-        for (var i = 0; i < this.vectors.length - 1; i ++) {
-            push()
-            translate(width / 2, height / 2)
-            line(this.vectors[i].x, this.vectors[i].y, this.vectors[i + 1].x, this.vectors[i + 1].y)
-            pop()
+        push()
+        translate(width / 2, height / 2)
+        this.faces.forEach((face) => {
+            face.vectors.map(vecoter => vecoter.update())
+            face.vectors.reduce((a, b) => {
+                line(a.pos2d.x, a.pos2d.y, b.pos2d.x, b.pos2d.y)
+                text(`${Math.floor(a.pos2d.x)}, ${Math.floor(a.pos2d.y)}`, a.pos2d.x + 4, a.pos2d.y + 4)
+                return b
+            })
+        })
+        for(let i = 0; i < 4; i ++) {
+            line(this.faces[0].vectors[i].pos2d.x, 
+                this.faces[0].vectors[i].pos2d.y,
+                this.faces[1].vectors[i].pos2d.x, 
+                this.faces[1].vectors[i].pos2d.y)
         }
+        pop()
     }
 }
 
 window.setup = () => {
     createCanvas(co.w, co.h)
-    background(200)
-    cubes.push(new Cube(100))
-    cubes[0].draw()
+    // frameRate(3)
+    cubes.push(new Cube(200))
+    setInterval(() => {
+        cubes.push(new Cube(200))
+    }, 3000)
+    // noLoop()
 }
 
 window.draw = () => {
-
+    background(200)
+    cubes.forEach(cube => {
+        cube.draw()
+    })
 }
 }
