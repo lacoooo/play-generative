40f1e34de6abd273ff801701edbdbc6fa028fa1a
Neko <469867876@qq.com>
2019-09-04 09:24:50 +0800

    6

--- 2d/2dSphere/main.js
+++ 2d/2dSphere/main.js
@@ -4,20 +4,20 @@ var Vertex = function(x, y, z) {
     this.z = parseFloat(z);
 };
 
-var Cube = function(center, side) {
+var Cube = function(side) {
     // Generate the vertices
     var d = side / 2;
 
     this.vertices = [
-        new Vertex(center.x - d, center.y - d, center.z + d),
-        new Vertex(center.x - d, center.y - d, center.z - d),
-        new Vertex(center.x + d, center.y - d, center.z - d),
-        new Vertex(center.x + d, center.y - d, center.z + d),
+        new Vertex( - d,  - d,  + d),
+        new Vertex( - d,  - d,  - d),
+        new Vertex( + d,  - d,  - d),
+        new Vertex( + d,  - d,  + d),
 
-        new Vertex(center.x + d, center.y + d, center.z + d),
-        new Vertex(center.x + d, center.y + d, center.z - d),
-        new Vertex(center.x - d, center.y + d, center.z - d),
-        new Vertex(center.x - d, center.y + d, center.z + d)
+        new Vertex( + d,  + d,  + d),
+        new Vertex( + d,  + d,  - d),
+        new Vertex( - d,  + d,  - d),
+        new Vertex( - d,  + d,  + d)
     ];
 
     // Generate the faces
@@ -35,8 +35,10 @@ function render(object, ctx, dx, dy) {
     var index = 0
     // Clear the previous frame
     ctx.clearRect(0, 0, 2 * dx, 2 * dy);
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.1)';
+    ctx.fillRect(0, 0, 2 * dx, 2 * dy);
     ctx.save()
-    ctx.translate(dx, 0)
+    ctx.translate(dx, dy)
     for (var i = 0; i < object.faces.length; i++) {
         // Current face
         var face = object.faces[i];
@@ -44,21 +46,31 @@ function render(object, ctx, dx, dy) {
         ctx.beginPath();
         // if (i > 0) continue
         // Draw the other vertices
+        var px = 0, py = 0
         for (var j = 0; j < face.length; j++) {
             index ++
             // console.log(index)
             var P = face[j]
+            var x = P.x 
+            * .6 + P.x * .4 * P.z / 200
+            var y = P.y 
+            // * .6 + P.x * .4 * P.z / 200
             if (j === 0) {
-                ctx.moveTo(P.x, P.y);
+                ctx.moveTo(px, py);
             }
             // console.log(P.x, -P.z)
-            ctx.lineTo(P.x, P.y);
+            ctx.arcTo(px, py, x + 4, y + 4, 10);
+            ctx.lineTo(x, y);
+            px = x
+            py = y
         }
 
         // Close the path and draw the face
         ctx.closePath();
+        ctx.strokeStyle = `hsla(${255}, ${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 100)}, 1)`;
+        ctx.lineWidth = Math.random() * 30
         ctx.stroke();
-        // ctx.fill();
+        ctx.fill();
     }
     ctx.restore()
 }
@@ -73,12 +85,8 @@ function render(object, ctx, dx, dy) {
 
     // object style
     var ctx = canvas.getContext('2d');
-    ctx.strokeStyle = 'rgba(0, 0, 0, 1)';
-    // ctx.fillStyle = 'rgba(255, 255, 255, 0.3)';
 
-    // Create the cube
-    var cube_center = new Vertex(0, dy, 0);
-    window.cube = new Cube(cube_center, dy);
+    window.cube = new Cube(dy);
     var object = window.cube;
 
     // First render
@@ -91,7 +99,7 @@ function render(object, ctx, dx, dy) {
     document.addEventListener('mousemove', move);
 
     // Rotate a vertice
-    function rotate(M, center, theta, phi) {
+    function rotate(M, theta, phi) {
         // Rotation matrix coefficients
         var ct = Math.cos(theta);
         var st = Math.sin(theta);
@@ -99,25 +107,23 @@ function render(object, ctx, dx, dy) {
         var sp = Math.sin(phi);
 
         // Rotation
-        var x = M.x - center.x;
-        var y = M.y - center.y;
-        var z = M.z - center.z;
+        var x = M.x
+        var y = M.y
+        var z = M.z
 
-        M.x = ct * x - st * cp * y + st * sp * z + center.x;
-        M.y = st * x + ct * cp * y - ct * sp * z + center.y;
-        M.z = sp * y + cp * z + center.z;
+        M.x = ct * x - st * cp * y + st * sp * z
+        M.y = st * x + ct * cp * y - ct * sp * z
+        M.z = sp * y + cp * z
     }
 
     function move(evt) {
-        var theta = (evt.clientX - mx) * Math.PI / 180;
-        var phi = (evt.clientY - my) * Math.PI / 180;
-
-        for (var i = 0; i < 8; ++i)
-            rotate(cube.vertices[i], cube_center, theta, phi);
+        var theta = (evt.clientX - mx) * Math.PI / 180 / 4;
+        var phi = (evt.clientY - my) * Math.PI / 180 / 4;
+        for (var i = 0; i < 8; i++)
+            rotate(cube.vertices[i], theta, phi);
 
         mx = evt.clientX;
         my = evt.clientY;
-
         render(object, ctx, dx, dy);
     }
 
